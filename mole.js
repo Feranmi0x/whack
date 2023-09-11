46b598849de9c0ae5b0fc92b89427ada14de53e8
Feranmi Omodara <140534541+Feranmi0x@users.noreply.github.com>
2023-09-11 16:18:12 +0100

    Add files via upload

---
+++ mole.js
@@ -0,0 +1,29 @@
+let currMoleTile;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    for (let i = 0; i < 9; i++) {
+    let tile = document.createElement("div");
+    tile.id = i.toString;
+    document.getElementById("board").appendChild(tile);
+    }
+
+    setInterval(setMol, 2000); 
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole() {
+    let mole = document.createElement("img");
+    mole.src = "./monty-mole.png";
+    
+    let num = getRandomTile();
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
